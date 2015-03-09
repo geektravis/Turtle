e29502c325e3fb00b64e6b96d9b03005c8a4019e
Travis Raybold <travis@raybold.com>
2015-03-08 20:46:16 -0700

    display and bind parameters for commands - using ng-switch for now, would love to get rid of that

--- src/app/logo/commandDisplay.directive.js
+++ src/app/logo/commandDisplay.directive.js
@@ -2,6 +2,5 @@ angular.module('logo').directive('commandDisplay', function () {
     return {
         templateUrl: 'logo/commandDisplay.directive.tpl.html',
         replace: true
-//        template: 'HELLO!'
     };
 });
