de53c0e85ca33f08d213a6423aefe3fdaea45965
Travis Raybold <travis@raybold.com>
2015-03-13 20:51:29 -0700

    add tests for commandList TURTLE-12

--- src/app/logo/commandList.spec.js
+++ src/app/logo/commandList.spec.js
@@ -1,27 +1,52 @@
-//describe('rectangle', function() {
-//  beforeEach(module('templates-app'));
-//  beforeEach(module('common'));
-//  beforeEach(module('rectangle'));
-//
-//  describe('rectangles service', function() {
-//    describe('add', function() {
-//      it("should add rectangle objects", inject(function(rectangles) {
-//        rectangles.add(1, 2);
-//
-//        var rectangle = rectangles.list()[0];
-//        expect(rectangle).property('width').to.equal(1);
-//        expect(rectangle).property('height').to.equal(2);
-//        expect(rectangle).to.respondTo('area');
-//      }));
-//    });
-//
-//    describe('list', function() {
-//      it("should hold a list of rectangles", inject(function(rectangles) {
-//        rectangles.add(1, 2);
-//        rectangles.add(3, 4);
-//
-//        expect(rectangles.list()).to.have.length(2);
-//      }));
-//    });
-//  });
-//});
+describe('logo', function () {
+    beforeEach(module('common'));
+    beforeEach(module('logo'));
+
+    describe('commandList', function () {
+        describe('addMove', function () {
+            it("should add a move command", inject(function (commandList) {
+                var move = commandList.addMove(123);
+
+                expect(move.distance).to.equal(123);
+                expect(commandList.list()[0]).to.equal(move);
+                expect(commandList.list().length).to.equal(1);
+            }));
+        });
+        describe('addLeft', function () {
+            it("should add a move command", inject(function (commandList) {
+                var left = commandList.addLeft(90);
+
+                expect(left.degrees).to.equal(90);
+                expect(commandList.list()[0]).to.equal(left);
+                expect(commandList.list().length).to.equal(1);
+            }));
+        });
+        describe('addRight', function () {
+            it("should add a move command", inject(function (commandList) {
+                var right = commandList.addRight(45);
+
+                expect(right.degrees).to.equal(45);
+                expect(commandList.list()[0]).to.equal(right);
+                expect(commandList.list().length).to.equal(1);
+            }));
+        });
+        describe('deleteCommand', function () {
+            it("should add a move command", inject(function (commandList) {
+                var right = commandList.addRight(45);
+                var move = commandList.addMove(123);
+                var left = commandList.addLeft(90);
+                var move2 = commandList.addMove(234);
+                console.log(commandList.list());
+
+                commandList.deleteCommand(2);
+                expect(commandList.list().length).to.equal(3);
+                expect(commandList.list()[0]).to.equal(right);
+                expect(commandList.list()[1]).to.equal(move);
+                expect(commandList.list()[2]).to.equal(move2);
+
+
+            }));
+        });
+
+    });
+});
