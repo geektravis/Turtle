0d5fd8283caf6dd4b8a88a554494c1396582d8f3
Travis Raybold <travis@raybold.com>
2015-03-14 21:22:31 -0700

    fix small bug

--- src/app/logo/logo.controller.js
+++ src/app/logo/logo.controller.js
@@ -1,28 +1,28 @@
-angular.module('logo').controller('LogoController', function ($state, $scope, commandList) {
-    console.log('Logo controller');
-    $scope.commands = commandList.list();
-//    $scope.addMove = commandList.addMove;
-    $scope.addMove = function (distance) {
-        var command = commandList.addMove(distance);
-    };
-    $scope.addLeft = function (degrees) {
-        return commandList.addLeft(degrees);
-    };
-    $scope.addRight = function (degrees) {
-        return commandList.addRight(degrees);
-    };
-    function dragControlListeners() {
-        return {
-            accept: function (sourceItemHandleScope, destSortableScope) {
-                return true;
-            },
-            itemMoved: function (event) {
-            },
-            orderChanged: function (event) {
-            },
-            containment: '#board'
+    angular.module('logo').controller('LogoController', function ($state, $scope, commandList) {
+        console.log('Logo controller');
+        $scope.commands = commandList.list();
+    //    $scope.addMove = commandList.addMove;
+        $scope.addMove = function (distance) {
+            return commandList.addMove(distance);
         };
-    }
+        $scope.addLeft = function (degrees) {
+            return commandList.addLeft(degrees);
+        };
+        $scope.addRight = function (degrees) {
+            return commandList.addRight(degrees);
+        };
+        function dragControlListeners() {
+            return {
+                accept: function (sourceItemHandleScope, destSortableScope) {
+                    return true;
+                },
+                itemMoved: function (event) {
+                },
+                orderChanged: function (event) {
+                },
+                containment: '#board'
+            };
+        }
 
 
-});
+    });
